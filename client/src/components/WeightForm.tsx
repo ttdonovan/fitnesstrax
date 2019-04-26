4e5ce867a85eb7a1310fdee16899e9b740d5b1eb
Savanni D'Gerinel <savanni@luminescent-dreams.com>
2019-04-26 09:37:05 -0400

    Add a type signature to the weight form

--- client/src/components/WeightForm.tsx
+++ client/src/components/WeightForm.tsx
@@ -1,22 +1,26 @@
-import React from "react";
-import math from "mathjs";
+import React from "react"
+import math from "mathjs"
 
-import { isSomething, renderWeight, parseUnit } from "../common";
-import { TextEditForm } from "./ValidatedText";
+import { isSomething, renderWeight, parseUnit } from "../common"
+import { TextEditForm } from "./ValidatedText"
 
-export const WeightForm = ({ value }: { value: any }) =>
-  isSomething(this.props.value) ? (
-    <p> {renderWeight(this.props.value.weight)} </p>
-  ) : (
-    <p> </p>
-  );
+interface Props {
+  value: any
+}
+
+export const WeightForm: React.SFC<Props> = ({ value }: Props) => (
+  <React.Fragment>
+    isSomething(this.props.value) ? (
+    <p> {renderWeight(this.props.value.weight)} </p>) : (<p> </p>)
+  </React.Fragment>
+)
 
 export const WeightEditForm = ({
   value,
-  onUpdate
+  onUpdate,
 }: {
-  value: any;
-  onUpdate: any;
+  value: any
+  onUpdate: any
 }) => (
   <TextEditForm
     value={this.props.value ? this.props.value.weight : math.unit(0, "kg")}
@@ -24,4 +28,4 @@ export const WeightEditForm = ({
     parse={parseUnit}
     onUpdate={value => this.props.onUpdate(value)}
   />
-);
+)
