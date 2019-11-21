6da3a73f6233046e97e6becafc229fe5ca73071e
Savanni D'Gerinel <savanni@luminescent-dreams.com>
2019-11-21 17:01:02 -0500

    Non-working minimal app

--- gtk/src/main.rs
+++ gtk/src/main.rs
@@ -5,71 +5,14 @@ extern crate gio;
 extern crate gtk;
 extern crate serde;
 
-use chrono::{TimeZone, Utc};
 use gio::prelude::*;
 use gtk::prelude::*;
 use gtk::BoxExt;
-use serde::{Deserialize, Serialize};
-use std::env;
-use std::error;
-use std::fmt;
-use std::fs::File;
-use std::io;
-use std::path;
-use std::result;
 use std::sync::{Arc, RwLock};
-
-use fitnesstrax;
-use fitnesstrax_gtk;
-
-#[derive(Debug)]
-pub enum Error {
-    TraxError(fitnesstrax::Error),
-    IOError(io::Error),
-}
-
-impl From<fitnesstrax::Error> for Error {
-    fn from(error: fitnesstrax::Error) -> Self {
-        Error::TraxError(error)
-    }
-}
-
-impl From<io::Error> for Error {
-    fn from(error: io::Error) -> Self {
-        Error::IOError(error)
-    }
-}
-
-impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            Error::TraxError(err) => write!(f, "Trax encountered an error: {}", err),
-            Error::IOError(err) => write!(f, "IO Error: {}", err),
-        }
-    }
-}
-
-impl error::Error for Error {
-    fn description(&self) -> &str {
-        match self {
-            Error::TraxError(err) => err.description(),
-            Error::IOError(err) => err.description(),
-        }
-    }
-
-    fn cause(&self) -> Option<&(dyn error::Error + 'static)> {
-        match self {
-            Error::TraxError(ref err) => Some(err),
-            Error::IOError(ref err) => Some(err),
-        }
-    }
-}
-
-type Result<A> = result::Result<A, Error>;
 
 #[derive(Clone)]
 struct AppContext {
-    count: Arc<RwLock<u32>>,
+    count: u32,
 }
 
 impl AppContext {
@@ -80,13 +23,11 @@ impl AppContext {
     }
 
     fn increment(&mut self) {
-        let mut val = self.count.write().unwrap();
-        *val = *val + 1;
+        self.val = self.val + 1;
     }
 
     fn decrement(&mut self) {
-        let mut val = self.count.write().unwrap();
-        *val = *val - 1;
+        self.val = self.val - 1
     }
 }
 
@@ -94,12 +35,16 @@ struct IncCtx {
     ctx: AppContext,
 }
 
-impl Fn for IncCtx {
+impl Fn<Args> for IncCtx
+where
+    Args: Display,
+{
     fn call(&self, args: Args) {
         self.ctx.increment();
     }
 }
 
+/*
 struct DecCtx {
     ctx: AppContext,
 }
@@ -109,6 +54,7 @@ impl DecCtx {
         self.ctx.decrement();
     }
 }
+*/
 
 fn main() {
     let application = gtk::Application::new(
@@ -144,54 +90,6 @@ fn main() {
 
         window.show_all();
     });
-
-    /*
-    application.connect_activate(move |app| {
-        let window = gtk::ApplicationWindow::new(app);
-        window.set_title("Fitnesstrax");
-        window.set_default_size(350, 70);
-
-        let main_box = gtk::Box::new(gtk::Orientation::Horizontal, 5);
-
-        let left_panel = gtk::Box::new(gtk::Orientation::Vertical, 5);
-        left_panel.add(&gtk::Label::new(Some("date select panel")));
-
-        let no_adjustment: Option<&gtk::Adjustment> = None;
-        let scrolling_history = gtk::ScrolledWindow::new(no_adjustment, no_adjustment);
-        let history_panel = gtk::Box::new(gtk::Orientation::Vertical, 5);
-        scrolling_history.add(&history_panel);
-
-        main_box.add(&left_panel);
-        main_box.pack_end(&scrolling_history, true, true, 5);
-
-        let range = (*ctx.range.read().unwrap()).clone();
-        let history = fitnesstrax_gtk::group_by_date(
-            range.clone(),
-            ctx.trax
-                .read()
-                .unwrap()
-                .get_history(
-                    emseries::DateTimeTz(range.start.and_hms(0, 0, 0)),
-                    emseries::DateTimeTz(range.end.and_hms(0, 0, 0)),
-                )
-                .unwrap(),
-        );
-
-        let mut dates = fitnesstrax_gtk::dates_in_range(range);
-        dates.sort();
-
-        for date in dates.into_iter() {
-            history_panel.add(&fitnesstrax_gtk::day_c(
-                &date,
-                history.get(&date).unwrap_or(&vec![]),
-            ));
-        }
-
-        window.add(&main_box);
-
-        window.show_all();
-    });
-    */
 
     application.run(&[]);
 }
